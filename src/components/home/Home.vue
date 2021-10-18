7c959a48d6a7b8be57a41bbb45511ec2006ea06f
GeovaniBueno1972 <you@example.comgeovani.bueno@yahoo.com.br>
2021-10-17 23:08:00 -0300

    versao 1

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -3,14 +3,12 @@
         <h1>Tela inicial</h1>
         <a href="/admin">Tela de Administração</a>
         <a href="/">Sair</a>
-        <Content />
-        <Footer />
+        
     </div>
 </template>
 
 <script>
-import Content from '../template/Content.vue'
-import Footer from '../template/Footer.vue'
+
 export default {
     name: 'Home',
     components: {Content, Footer}
